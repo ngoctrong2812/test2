938870fcb6336dd998ec77ceed0fb2c4d9ee8c82
DESKTOP-T5FSIUL\ACER A315 <ngoctuyen122889@gmail.com>
2021-04-10 12:00:44 +0700

    gffg

---
+++ src/test2/index.java
@@ -0,0 +1,25 @@
+package test2;
+
+import java.util.Scanner;
+
+public class index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap So a :");
+		a = sc.nextInt();
+		switch (a) {
+		case 1: {
+			
+			System.out.print("DEMO");
+			break;
+		}
+		default:
+			System.out.print("test");
+		}
+		
+	}
+
+}
